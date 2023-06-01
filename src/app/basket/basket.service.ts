d8a618acb4990753cf1afa16b28243ac560f25e4
AhmedFrikhaa <ahmed.frikha2@insat.ucar.tn>
2023-06-01 21:53:36 +0200

    basket

--- src/app/basket/basket.service.ts
+++ src/app/basket/basket.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {Basket} from "../shared/models/basket";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
+  private baseUrl = '/api/basket';
+  basket: Observable<Basket>;
+  constructor(private http:HttpClient) { }
+  getBasket(): Observable<Basket> {
+    return this.http.get<Basket>(`${this.baseUrl}`);
+  }
+  decreaseQuantity(item: any) {
+    if (item.quantity > 1) {
+      item.quantity--;
+    }
+  }
 
-  constructor() { }
+  increaseQuantity(item: any) {
+    item.quantity++;
+  }
+
+  updateBasket(basket: Basket): Observable<Basket> {
+    return this.http.put<Basket>(`${this.baseUrl}`, basket);
+  }
+  removeItemFromBasket(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+  getPricePerItem(item: any,occurrence:number): number {
+    return item.price*occurrence;
+  }
 }
--- src/app/shared/models/order.ts
+++ src/app/shared/models/order.ts
@@ -1,16 +1,13 @@
-import { Address } from "./user";
 
 export interface OrderToCreate {
   basketId: string;
   deliveryMethodId: number;
-  shipToAddress: Address;
 }
 
 export interface Order {
   id: number;
   buyerEmail: string;
   orderDate: string;
-  shipToAddress: Address;
   deliveryMethod: string;
   shippingPrice: number;
   orderItems: OrderItem[];
