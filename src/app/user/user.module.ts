1684edc9c737de1781c14a07445a65c31bc9b380
AhmedFrikhaa <ahmed.frikha2@insat.ucar.tn>
2023-06-01 20:34:14 +0200

    login&register

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,7 +4,8 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import {UserRoutingModule} from "./user-routing.module";
 import { UserInfoComponent } from './user-info/user-info.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 
 
@@ -14,10 +15,10 @@ import {ReactiveFormsModule} from "@angular/forms";
     SignUpComponent,
     UserInfoComponent
   ],
-    imports: [
-        CommonModule,
-        UserRoutingModule,
-        ReactiveFormsModule
-    ]
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    FormsModule, HttpClientModule
+  ]
 })
 export class UserModule { }
